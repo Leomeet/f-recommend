eabad82648f666f2be2af5d6e471c85bde870fc8
Mitul Kalariya <mitul.k@simformsolutions.com>
2023-10-20 01:00:37 +0530

    updated: process and req.

--- process.py
+++ process.py
@@ -3,19 +3,18 @@ import pandas as pd
 from vectordb.pinecone import pinecone
 
 
+data_to_insert = 10
+
 formatting_columns = ["gender","masterCategory","subCategory","articleType","baseColour","season","year","usage","productDisplayName"]
 data_frame = pd.read_csv("data/styles.csv", on_bad_lines='skip')
-data_frame = data_frame.head(10)
+data_frame = data_frame.head(data_to_insert)
 data_frame = data_frame.applymap(str)
 data_frame['metadata'] = data_frame.apply(lambda row: json.loads(row.to_json()), axis=1)
 data_frame['text_content'] = data_frame.apply(lambda row: ' :: '.join(map(str, row[formatting_columns])), axis=1)
-print(data_frame)
-data_frame.to_csv("data/sample_10_items.csv")
+data_frame.to_csv(f"data/sample_{data_to_insert}_items.csv")
 
 df = data_frame
 op_columns = ["id","metadata","text_content"]
 list_of_rows = [row[op_columns].to_dict() for _, row in df.iterrows()]
 
 pinecone.upsert_documents(documents=list_of_rows)
-
-
